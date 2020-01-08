9c3a55e1d587fa43f308da616b181abf64045d27
TheCreator555 <56085484+TheCreator555@users.noreply.github.com>
2020-01-08 10:47:47 -0500

    e

--- WeatherProject/app/src/main/java/com/example/weatherproject/MainActivity.java
+++ WeatherProject/app/src/main/java/com/example/weatherproject/MainActivity.java
@@ -5,24 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.id_editText);
 
         AsyncThread asyncThread = new AsyncThread();
         asyncThread.execute();
@@ -37,17 +38,15 @@ public class MainActivity extends AppCompatActivity {
             try {
                 URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?appid=d165cb431bba386b434067ab6b0be227&units=imperial&zip=08852,us");
                 URLConnection urlConnection = url.openConnection();
-                Log.d("SHAH", "nigga");
                 InputStream inputStream = urlConnection.getInputStream();
-                Log.d("SHAH", "nigga1");
                 String json = "";
                 String line = null;
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 while ((line = bufferedReader.readLine()) != null)
                     json += line + "\n";
-                bufferedReader.close();
+                //bufferedReader.close();
                 JSONObject jsonObject = new JSONObject(json);
-                Log.d("SHAH", jsonObject.toString(2));
+                Log.d("TAG", jsonObject.toString(2));
             } catch (Exception e) {
                 e.printStackTrace();
             }
