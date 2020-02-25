92ddb9502d14a72cb6c76bc25ae1ecc134b22ebb
TheCreator555 <56085484+TheCreator555@users.noreply.github.com>
2020-02-24 21:12:22 -0500

    e

--- CookieClicker/app/src/main/java/com/example/cookieclicker/Main2Activity.java
+++ CookieClicker/app/src/main/java/com/example/cookieclicker/Main2Activity.java
@@ -7,27 +7,56 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main2Activity extends AppCompatActivity {
     Button clickerButton,buttonPurchase;
     private int dropletsPerSecond;
+    AtomicInteger droplets;
+    TextView textViewShopDroplets;
+    TextView textViewFaucetCost;
+    MyThread myThreadShop;
+    int cost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         clickerButton = findViewById(R.id.id_buttonClicker);
         buttonPurchase = findViewById(R.id.id_buttonPurchase);
+        textViewShopDroplets = findViewById(R.id.id_textView_ShopDroplets);
+        textViewFaucetCost = findViewById(R.id.id_textView_FaucetCost2);
+
+        droplets = new AtomicInteger();
+
 
         Intent intent = getIntent();
         dropletsPerSecond = intent.getIntExtra("DROPLETS_PER_SECOND",0);
+        droplets.set(intent.getIntExtra("DROPLETS",0));
+        textViewShopDroplets.setText("Droplets: " + droplets.get());
 
+        cost = (int)(20*(1.0+(double)(dropletsPerSecond)/3));
+        textViewFaucetCost.setText("Faucet: " + cost + " droplets");
 
+        myThreadShop = new MyThread();
+        myThreadShop.start();
         configureClickerButton();
 
         buttonPurchase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dropletsPerSecond+=5;
+                if(droplets.get() >= cost) {
+                    dropletsPerSecond += 1;
+                    droplets.set((droplets.get()-cost));
+                    Toast.makeText(Main2Activity.this, "Faucet Purchased!", Toast.LENGTH_SHORT).show();
+                    cost = (int)(20*(1.0+(double)(dropletsPerSecond)/3));
+                    textViewFaucetCost.setText("Faucet: " + cost + " droplets");
+                }
+                else{
+                    Toast.makeText(Main2Activity.this, "Insufficient Funds", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -37,9 +66,34 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent1 = new Intent(Main2Activity.this,MainActivity.class);
                 intent1.putExtra("DROPLETS_PER_SECOND",dropletsPerSecond);
+                intent1.putExtra("DROPLETS", droplets.get());
                 setResult(RESULT_OK,intent1);
                 finish();
             }
         });
+    }
+    public class MyThread extends Thread{
+        public void run(){
+            while(true) {
+                try {
+                    Thread.sleep(1000);
+                    droplets.addAndGet(dropletsPerSecond);
+                    textViewShopDroplets.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textViewShopDroplets.setText("Droplets: " + droplets);
+                        }
+                    });
+
+                } catch (Exception e) {
+                    System.out.println("error :(");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        return;
     }
 }
--- CookieClicker/app/src/main/java/com/example/cookieclicker/MainActivity.java
+++ CookieClicker/app/src/main/java/com/example/cookieclicker/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -17,51 +19,77 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    int xTouch;
-    int yTouch;
-    TextView clickText, dropletText, dropletsPerSecondText;
-    ImageView imageView;
-    ConstraintLayout constraintLayout;
-    Context context;
-    Button storeButton;
-    private int droplets = 0, dropletsPerSecond = 0;
+import com.squareup.picasso.Picasso;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MainActivity extends AppCompatActivity {
+    TextView clickText, dropletText, dropletsPerSecondText;
+    ImageView imageViewGlass, imageViewFaucet;
+    ConstraintLayout constraintLayout;
+    Button storeButton;
+    AtomicInteger droplets;
+    int dropletsPerSecond = 0;
+    MyThread myThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.id_imageView);
+        imageViewGlass = findViewById(R.id.id_imageView);
         constraintLayout = findViewById(R.id.id_layout);
         dropletText = findViewById(R.id.id_dropletTextView);
         dropletsPerSecondText = findViewById(R.id.id_dropletPerSecondTextView);
-        context = this;
         storeButton = findViewById(R.id.id_storeButton);
+        droplets = new AtomicInteger();
 
+        constraintLayout.setBackgroundColor(Color.BLACK);
         configureStoreButton();
+
+        final ImageView backgroundOne = findViewById(R.id.background_one);
+        final ImageView backgroundTwo = findViewById(R.id.background_two);
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float height = backgroundOne.getHeight();
+                final float translationY = height * progress;
+                backgroundOne.setTranslationY(translationY);
+                backgroundTwo.setTranslationY(translationY - height);
+            }
+        });
+        animator.start();
+
+
 
         final ScaleAnimation animationGlass = new ScaleAnimation(.75f, 1.0f, .75f,1.0f, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
         animationGlass.setDuration(400);
 
+        myThread = new MyThread();
+        myThread.start();
 
 
-
-        imageView.setOnClickListener(new View.OnClickListener() {
+        imageViewGlass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 v.startAnimation(animationGlass);
-                droplets++;
+                droplets.incrementAndGet();
 
-                clickText = new TextView(context);
+                clickText = new TextView(MainActivity.this);
                 clickText.setId(View.generateViewId());
                 clickText.setText("+1");
+                clickText.setTextColor(Color.WHITE);
 
                 ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
                 clickText.setLayoutParams(params);
@@ -76,50 +104,30 @@ public class MainActivity extends AppCompatActivity {
                 constraintSet.connect(clickText.getId(),constraintSet.BOTTOM,constraintLayout.getId(),constraintSet.BOTTOM);
                 constraintSet.connect(clickText.getId(),constraintSet.RIGHT,constraintLayout.getId(),constraintSet.RIGHT);
 
-                DisplayMetrics displayMetrics = new DisplayMetrics();
-                getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-                int height = displayMetrics.heightPixels;
-                int width = displayMetrics.widthPixels;
 
-                constraintSet.setHorizontalBias(clickText.getId(),(float)xTouch/width);
-                constraintSet.setVerticalBias(clickText.getId(),(float)yTouch/height);
+                constraintSet.setHorizontalBias(clickText.getId(),(float)(Math.random()*.5+.25));
+                constraintSet.setVerticalBias(clickText.getId(),.5f);
 
 
                 constraintSet.applyTo(constraintLayout);
 
-                //TranslateAnimation animationClick = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,200,Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,200);
-                AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f,0.0f);
-                //animationClick.setDuration(2000);
-                alphaAnimation.setDuration(2000);
+                TranslateAnimation animationClick = new TranslateAnimation(0,0,0,-600);
+                AlphaAnimation animationFade = new AlphaAnimation(1.0f,0.0f);
+                animationClick.setDuration(2000);
+                animationFade.setDuration(2000);
 
                 AnimationSet animationSet = new AnimationSet(false);
 
-                //animationSet.addAnimation(animationClick);
-                animationSet.addAnimation(alphaAnimation);
-
+                animationSet.addAnimation(animationClick);
+                animationSet.addAnimation(animationFade);
 
                 clickText.startAnimation(animationSet);
                 clickText.setVisibility(View.INVISIBLE);
 
-                dropletText.setText(droplets + " droplets");
+                dropletText.setText(droplets.get() + " droplets");
             }
         });
 
-    }
-
-    public class Item{
-        int dropletsPerSecond;
-        int image;
-        public Item(int dropletsPerSecond, int image){
-            this.dropletsPerSecond = dropletsPerSecond;
-            this.image = image;
-        }
-        public int getDropletsPerSecond(){
-            return dropletsPerSecond;
-        }
-        public int getImage(){
-            return image;
-        }
     }
 
     private void configureStoreButton(){
@@ -128,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,Main2Activity.class);
                 intent.putExtra("DROPLETS_PER_SECOND",dropletsPerSecond);
-
+                intent.putExtra("DROPLETS",droplets.get());
                 startActivityForResult(intent, 0);
             }
         });
@@ -140,8 +148,55 @@ public class MainActivity extends AppCompatActivity {
 
         if(requestCode == 0){
             if(resultCode == RESULT_OK){
-                dropletsPerSecond = data.getIntExtra("DROPLETS_PER_SECOND",0);
-                Log.d("ROHAN",dropletsPerSecond + "");
+                droplets.set(data.getIntExtra("DROPLETS", 0));
+                dropletText.setText(droplets.get() + " droplets");
+                while(data.getIntExtra("DROPLETS_PER_SECOND",0) > dropletsPerSecond){
+                    imageViewFaucet = new ImageView(MainActivity.this);
+                    imageViewFaucet.setId(View.generateViewId());
+                    Picasso.get().load(R.drawable.faucet).resize(175,175).into(imageViewFaucet);
+
+                    ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                    imageViewFaucet.setLayoutParams(params);
+
+
+                    constraintLayout.addView(imageViewFaucet);
+
+                    ConstraintSet constraintSet = new ConstraintSet();
+                    constraintSet.clone(constraintLayout);
+
+                    constraintSet.connect(imageViewFaucet.getId(),constraintSet.TOP,constraintLayout.getId(),constraintSet.TOP);
+                    constraintSet.connect(imageViewFaucet.getId(),constraintSet.LEFT,constraintLayout.getId(),constraintSet.LEFT);
+                    constraintSet.connect(imageViewFaucet.getId(),constraintSet.BOTTOM,constraintLayout.getId(),constraintSet.BOTTOM);
+                    constraintSet.connect(imageViewFaucet.getId(),constraintSet.RIGHT,constraintLayout.getId(),constraintSet.RIGHT);
+
+
+                    constraintSet.setHorizontalBias(imageViewFaucet.getId(),(float)(Math.random()*.5+.25));
+                    constraintSet.setVerticalBias(imageViewFaucet.getId(),(float)(Math.random()*.1+.85));
+
+
+                    constraintSet.applyTo(constraintLayout);
+                    dropletsPerSecond++;
+                }
+                dropletsPerSecondText.setText(dropletsPerSecond + " droplets/second");
+            }
+        }
+    }
+    public class MyThread extends Thread{
+        public void run(){
+            int x = 0;
+            while(x==0) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    System.out.println("error :(");
+                }
+                droplets.addAndGet(dropletsPerSecond);
+                dropletText.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        dropletText.setText(droplets + " droplets");
+                    }
+                });
             }
         }
     }
